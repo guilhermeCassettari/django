b0489dd19266bca6828372b7b1243aee06ab8a45
Guilherme Gasparotti Cassettari <guizaocassettari@gmail.com>
2023-11-03 16:51:03 -0300

    request response html

---
+++ recipes/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'recipes/home.html', {'name': 'Guilherme'})
+
+
+def contact(request):
+    return HttpResponse("Contato.")
+
+
+def about(request):
+    return HttpResponse("Sobre.")
